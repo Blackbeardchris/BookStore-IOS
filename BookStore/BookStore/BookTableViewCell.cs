6cf8c45528a405e8cfe6a192e0ce4b1c821e473a
Blackbeardchris <christopherl10@learn.cssd.ab.ca>
2017-10-19 14:00:23 -0600

    fdsf

---
+++ BookStore/BookStore/BookTableViewCell.cs
@@ -0,0 +1,30 @@
+using Foundation;
+using System;
+using UIKit;
+//controls the cell, to do whatever needed
+namespace BookStore
+{
+    public partial class BookTableViewCell : UITableViewCell
+    {
+        private Book bookData;
+        public Book BookData
+        {
+            get { return bookData; }
+            //when bookdata gets a value it looks at these below
+            set {
+                bookData = value;
+
+                bookAuthorLabel.Text = bookData.Author;
+                bookNameLabel.Text = bookData.Name;
+                bookYearLabel.Text = bookData.Year.ToString();
+                bookPublisherLabel.Text = bookData.Publisher;
+                //accessing the labels
+                    
+            }
+        }
+        public BookTableViewCell (IntPtr handle) : base (handle)
+        {
+            
+        }
+    }
+}
--- BookStore/BookStore/BooksTableViewController.cs
+++ BookStore/BookStore/BooksTableViewController.cs
@@ -1,15 +1,67 @@
 using Foundation;
 using System;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace BookStore
 {
-    public partial class BooksTableViewController : UITableViewController
+    partial class BooksTableViewController : UITableViewController
     {
         List<Book> bookList;
         public BooksTableViewController (IntPtr handle) : base (handle)
         {
             bookList = new List<Book>();
+            //add as many books
+            bookList.Add(new Book()
+            {
+                Author = "J.K. Rowling",
+                Name = "Harry Potter and the Sorcerer's Stone",
+                Publisher = "Bloomsbury",
+                Year = 1997
+            });
+
+			bookList.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Chamber of Secrets",
+				Publisher = "Bloomsbury",
+				Year = 1998
+			});
+
+			bookList.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Prisoner of Azkaban",
+				Publisher = "Bloomsbury",
+				Year = 1999
+			});
+        }
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+            //only one section needed
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return bookList.Count;
+            //counts the amount in the booklist
+        }
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("Book")as BookTableViewCell;
+            //uses the booktableviewcell
+            //grabs reusable cell
+
+            var data = bookList[indexPath.Row];
+                                 //current row
+            //data displayed in the cell
+
+            cell.BookData = data.Name;
+
+            return cell;
+            //retrieve cell and return to use in tableview
         }
     }
 
@@ -17,7 +69,7 @@ namespace BookStore
     {
         public string Name;
         public string Author;
-        public string Editor;
+        public string Publisher;
         public int Year;
     }
 }
